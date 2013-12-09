0a5999edce56d31519759b0fffff96ddebd88b0a
Luke <563723791@qq.com>
2013-12-09 23:47:44 +0800

    project create

---
+++ Resources/src/resource.js
@@ -0,0 +1,22 @@
+var s_HelloWorld = "res/HelloWorld.png";
+var s_CloseNormal = "res/CloseNormal.png";
+var s_CloseSelected = "res/CloseSelected.png";
+var s_dragon_png = "hd/dragon.png";
+var s_dragon_xml = "hd/dragon.xml";
+var s_dragon_plist = "hd/dragon.plist";
+var g_ressources = [
+    //image
+    {src:s_HelloWorld},
+    {src:s_CloseNormal},
+    {src:s_CloseSelected}
+
+    //plist
+
+    //fnt
+
+    //tmx
+
+    //bgm
+
+    //effect
+];
